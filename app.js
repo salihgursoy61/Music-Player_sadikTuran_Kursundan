91de091790f2fc53315ddc57a8edec4f3ffb27e8
salihgursoy61 <121294952+salihgursoy61@users.noreply.github.com>
2024-11-22 03:11:45 +0300

    müzik dizesi ve class ı olusturuldu, müzikler arası gecisler olsuturuldu

---
+++ app.js
@@ -0,0 +1,22 @@
+const player = new MusicPlayer(musicList);
+
+let music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+
+player.previous();
+music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+player.next();
+music = player.getMusic();
+console.log(music.getName());
---
+++ music.js
@@ -0,0 +1,17 @@
+class Music {
+  constructor(title, singer, img, file) {
+    this.title = title;
+    this.singer = singer;
+    this.img = img;
+    this.file = file;
+  }
+  getName() {
+    return this.title + " - " + this.singer;
+  }
+}
+
+const musicList = [
+  new Music("Boşver", "Nilüfer", "1.jpeg", "1.mp3"),
+  new Music("Bu da gecer mi sevgilim", "Yalın", "2.jpeg", "2.mp3"),
+  new Music("Aramızda Ucurumlar", "Suat Suna", "3.jpeg", "3.mp3"),
+];
